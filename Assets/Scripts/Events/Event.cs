af3780d7fd0cd22a3dac6638831ad4d0d9e9102b
Dmitry <dimon13142@mail.ru>
2015-11-27 20:37:05 +0300

    Начинаю писать систему событий

--- Assets/Scripts/Events/Event.cs
+++ Assets/Scripts/Events/Event.cs
@@ -3,8 +3,39 @@ using System.Collections;
 
 public class Event //UNDONE
 {	
-	float Probability;
+	// Описание события, выводимое на экран.
+	string Description;
 
+	// TODO Базовые возможности успеха реакции на событие, "0" - отсутствие такого варианта.
+	float RunAwayChance;
+	// и т.п.
+
+	// Базовая вероятность возникновения события, на него будет влиять параметр удачи персонажа.
+	float Probability;
+	// Определяет, "хорошее" это событие или "плохое", необходимо для правильного влияния параметра удачи.
+	bool Good;
+
+	// TODO Коэффициент вероятности возникновения события в типе местности, "0" - событие не происходит в этом типе.
 	float ForestProb;
 	float RiverProb;
+	// и т.п.
+
+	// TODO Результаты события.
+	string ResultDescription;
+	float HealthEffect;
+	float StaminaEffect;
+	// и т.п.
+
+	public Event (string description, float runAwayChance, float probability, bool good, float forestProb, float riverProb, string resultDescription, float healthEffect, float staminaEffect)
+	{
+		Description = description;
+		RunAwayChance = runAwayChance;
+		Probability = probability;
+		Good = good;
+		ForestProb = forestProb;
+		RiverProb = riverProb;
+		ResultDescription = resultDescription;
+		HealthEffect = healthEffect;
+		StaminaEffect = staminaEffect;
+	}
 }
--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -1,18 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
-public class EventManager : MonoBehaviour {
-
+public class EventManager : MonoBehaviour
+{
 	public GameObject EventTimer;
 	public GameObject EventPanel;
+	public string EventsFilename;
+	List<Event> Events;
 
-	// Use this for initialization
-	void Start () {
-	
+	public void MakeActionEvent ()
+	{
+		//TODO Событие при переходе игрока на тайл.
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void MakeTimeEvent ()
+	{
+		//TODO Событие от таймера.
+	}
+
+	void LoadEventsFromFile ()
+	{
+		//TODO Загрузка события из бинарного файла.
+		if (File.Exists (EventsFilename)) 
+		{
+			using (BinaryReader reader=new BinaryReader(File.Open(EventsFilename,FileMode.Open))) 
+			{
+				while (reader.PeekChar()!=-1)
+				{
+					Events.Add (new Event (
+						reader.ReadString (),
+						reader.ReadSingle (),
+						reader.ReadSingle (),
+						reader.ReadBoolean (),
+						reader.ReadSingle (),
+						reader.ReadSingle (),
+						reader.ReadString (),
+						reader.ReadSingle (),
+						reader.ReadSingle ()
+					));
+				}
+			}
+		} 
+		else
+			throw new IOException ("File with events do not exist.");
 	}
 }
