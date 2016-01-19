981ca027875cc36b7ea27604c705cf5598703b85
Dmitry <dimon13142@mail.ru>
2016-01-19 16:05:55 +0300

    Разное

--- Assets/Scripts/Entities/Creature.cs
+++ Assets/Scripts/Entities/Creature.cs
@@ -1,62 +1,108 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Creature : Entity 
+public class Creature : Entity
 {
+	public enum AI_State{STATE_IDLE, STATE_MOVE, STATE_ATTACK};
+
 	public GameObject World; //TODO Возможно, надо будет перенести выше по иерархии наследования.
 
 	public float MoveAnimationTime;
 
-	public float Health;	
-
+	public float MaxHealth;
+	public float Health;
+	
+	public float Damage;
+	public float Armor;
+	//public float Experience;
+	
 	bool Moving;
 	Vector2 TargetCoords;
 	float MoveTime;
 	Vector2 PreviousCoords;
+	AI_State State;
+	GameObject Target;
 
-
-	void Update()
+	protected void Update ()
 	{
-		if(Moving)
+		if (Moving) 
 		{
-		MoveTime-=Time.deltaTime;
-			float tstep=MoveTime/Time.deltaTime;
-			float dstep=Vector2.Distance(transform.position,World.GetComponent<WorldVisualiser>().GetTransformPosFromMapCoords(MapCoords))/tstep;
-		//GetComponent<SpriteRenderer>().sprite=MoveSprite;
-			if(MoveTime>0)//(Vector2)transform.position!=World.GetComponent<WorldVisualiser>().GetTransformPosFromMapCoords(MapCoords))
-			{
-				transform.position= Vector2.MoveTowards(transform.position, World.GetComponent<WorldVisualiser>().GetTransformPosFromMapCoords(MapCoords),dstep);
-			}
-			else
-			{
-			Moving=false;
-		//GetComponent<SpriteRenderer>().sprite=IdleSprite;
-			}
+			MoveTime -= Time.deltaTime;
+			float tstep = MoveTime / Time.deltaTime;
+			float dstep = Vector2.Distance (transform.position, World.GetComponent<WorldVisualiser> ().GetTransformPosFromMapCoords (MapCoords)) / tstep;
+			if (MoveTime > 0) 
+				transform.position = Vector2.MoveTowards (transform.position, World.GetComponent<WorldVisualiser> ().GetTransformPosFromMapCoords (MapCoords), dstep);
+			else 
+				Moving = false;
 		}
 	}
 
-	public void MoveToMapCoords(Vector2 mapCoords)
+	public void MoveToMapCoords (Vector2 mapCoords)
 	{
-		TargetCoords=mapCoords;
+		State = AI_State.STATE_MOVE;
+		TargetCoords = mapCoords;
 	}
 
-	void Move()
+	public void Attack (GameObject target)
 	{
-		sbyte dx=(sbyte)(TargetCoords.x-MapCoords.x);
-		sbyte dy=(sbyte)(TargetCoords.y-MapCoords.y);
-
-		if(dx!=0)
-			dx=(sbyte)(dx>0?1:-1);
-		if(dy!=0)
-			dy=(sbyte)(dy>0?1:-1);
-		MapCoords.x +=dx;
-		MapCoords.y+=dy;
-		MoveTime=MoveAnimationTime;
-		Moving=true;
+		State = AI_State.STATE_ATTACK;
+		Target = target;
 	}
-	
-	public void MakeTurn()
+
+	public void Idle ()
 	{
-		Move();
+		Target = null;
+		State = AI_State.STATE_IDLE;
+	}
+
+	void Move ()
+	{
+		sbyte dx = (sbyte)(TargetCoords.x - MapCoords.x);
+		sbyte dy = (sbyte)(TargetCoords.y - MapCoords.y);
+
+		if (dx != 0)
+			dx = (sbyte)(dx > 0 ? 1 : -1);
+		if (dy != 0)
+			dy = (sbyte)(dy > 0 ? 1 : -1);
+		MapCoords.x += dx;
+		MapCoords.y += dy;
+		MoveTime = MoveAnimationTime;
+		Moving = true;
+	}
+
+	void PerformAttack ()
+	{
+		Target.GetComponent<Creature> ().TakeDamage (Damage);
+	}
+
+	public void TakeDamage (float damage)
+	{
+		Health -= Mathf.Clamp (damage - Armor, 0, damage);
+	}
+
+	public void MakeTurn ()
+	{
+		switch (State) 
+		{
+		case AI_State.STATE_IDLE:
+			break;
+		case AI_State.STATE_MOVE:
+			if (TargetCoords == MapCoords)
+				Idle ();
+			else
+				Move ();
+			break;
+		case AI_State.STATE_ATTACK:
+			if (World.GetComponent<World> ().IsMapCoordsAdjacent (TargetCoords, MapCoords)) 
+			{
+				PerformAttack ();
+			}
+			else 
+			{
+				TargetCoords = Target.GetComponent<Creature> ().MapCoords;
+				Move ();
+			}
+			break;
+		}
 	}
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,11 +3,78 @@ using System.Collections;
 
 public class Player : Creature 
 {
+	//[Header("Текст на панели характеристик")]
+	//public Text CurrentHealthText;
+	//public Text CurrentEnergyText;
+	//public Text CurrentStaminaText;
+	//public Text CurrentHungreedText;
+	
+	[Header("Факторы влияющие на характеристики")]
+	public float HungerTime = 20;  // Переменная времени голода (через сколько секунд отнимаем еду)
+	public float HungerTimeSet = 20; // Переменная времени голода (для повтора отнимания еды); надо переделать
+	
+	
+	[Header("Основные характеристики")]
+	public float MaxEnergy = 100; // Максимальное количество энергии
+	public float MaxStamina = 1000; // Максимальное количество силы
+	public float MaxHungreed = 500; // Максимальное количество еды
+	
+	public float CurrentEnergy = 100; // Энергия (в данный момент)
+	public float CurrentStamina = 1000; // Выносливость (сколько в данный момент)
+	public float CurrentHungreed = 500; // Голод (в данный момент)
+	
+	[Header("Дальность обзора")]
 	public byte ViewDistance;
 
 	void Start()
 	{
 		MapCoords=new Vector2(5,5);
 		GetComponent<SpriteRenderer>().sortingLayerName="Player";
+	}
+
+	void Update()
+	{
+		base.Update();
+
+		if(Health<=0){
+			Health=0;
+			Application.LoadLevel("GameOver");
+		}
+		if(CurrentEnergy<=0){ //Если жизни меньше или равно 0
+			CurrentEnergy=0; // Задаём значение 0
+		}
+		if(HungerTime>0){  // Если HungerTime больше нуля
+			HungerTime -=Time.deltaTime; //Отсчитываем в минус
+		}
+		if(HungerTime<0){ //Если HungerTime меньше нуля
+			CurrentHungreed -=1; // Отбавляем еду
+			HungerTime = HungerTimeSet; //Повторяем всё выше написанное
+		}
+		if(CurrentHungreed==-1){ //Если еда равна -1
+			CurrentHungreed=0; // Задаём значение 0
+		}
+		if(CurrentHungreed<=0){ //Если еда меньше или равна 0
+			CurrentEnergy -= 0.0025f; // Отнимаем здоровье
+		}
+		if(CurrentStamina != 1000){ // Если выносливость не равна 1000
+			CurrentStamina +=0.0025f; // Прибавляем выносливость (Отдых)
+		}
+		if(Input.GetKey (KeyCode.LeftShift)){ // Если нажать кнопку LeftShift (Для теста)
+			CurrentStamina -= 1; // Отнимаем выносливлость
+		}
+		if(CurrentStamina == 1000){ //Если выносливость равна 1000
+			CurrentStamina = MaxStamina; // Задаём число значения MaxStamina
+		}
+		if(CurrentStamina<=0){ // Если выносливость меньше или равна 0
+			CurrentStamina=0; // Задаём число 0
+		}
+		
+		
+		
+		//Наверное надо куда-нибудь переместить :)
+		//	CurrentHealthText.text = "Health: " + Health + " / " + MaxHealth;
+		//CurrentEnergyText.text = "Energy: " + CurrentEnergy + " / " + MaxEnergy;
+		//CurrentStaminaText.text = "Stamina: " + CurrentStamina + " / " + MaxStamina;
+		//CurrentHungreedText.text = "Hungreed: " + CurrentHungreed + " / " + MaxHungreed;
 	}
 }
